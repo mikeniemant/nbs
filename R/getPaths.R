8c3df58c5be74b79ee9ae9a5d6968a487656339a
mikeniemant <michael_niemantsverdriet@hotmail.com>
2020-01-05 12:17:49 +0100

    Added find TODO statement function to getPaths

--- R/getPaths.R
+++ R/getPaths.R
@@ -2,15 +2,19 @@
 #'
 #' This function sets all paths
 #'
-#' Version 1.5 2019-11-04
+#' Version 1.6 2020-01-05
 #'
 #' @param main.path Main path of the work environment
 #' @keywords getPaths
 #' @export
-#' @examples
+#' @param main.path = Main path of project
+#' @param report.name = Report name
+#' @param type = Type of document for a specific project directory
+#' @param todo = Boolean to find all TODO: cases in the code directory
+#' @examples PATHS <- getPaths(main.path = "/Users/michaelniemantsverdriet/Library/Mobile Documents/com~apple~CloudDocs/workspace/000/4_data_science_in_practice/")
 #' getPaths()
 
-getPaths <- function(main.path, report.name = NA, type = "report") {
+getPaths <- function(main.path, report.name = NA, type = "report", todo = T) {
   if(substr(main.path, nchar(main.path), nchar(main.path)) != "/") {
     main.path <- paste0(main.path, "/")
   }
@@ -45,6 +49,41 @@ getPaths <- function(main.path, report.name = NA, type = "report") {
       path.list$modeling_results = paste0(path.list$modeling_report, "results/")
     }
   }
-
+  
+  # find todo function
+  if(todo) {
+    # Define dir and search pattern for specific file types
+    dir <- path.list$main
+    search.pattern <- paste0("(", paste(c("r", "R", "Rmd", "rmd"), collapse = "|"),
+                             ")$")
+    
+    # Identify related files
+    rel.docs <- list.files(path = dir, recursive = T, pattern = search.pattern)
+    
+    # Iteratively search in all relevant files for TODO statement
+    for (doc in rel.docs) {
+      dir.doc <- paste0(dir, "/", doc)
+      file_contents <- readLines(dir.doc)
+      
+      pattern.lines <- NULL
+      for (i in 1:length(file_contents)) {
+        if (grepl(pattern = "TODO:", x = file_contents[i]))
+          pattern.lines <- c(pattern.lines, i)
+      }
+      if (length(pattern.lines) != 0) {
+        for (i in pattern.lines) {
+          mock.line <- file_contents[(i - 1):(i + 1)]
+          
+          cat(
+            paste0("TODO found in line ", i, " in file ",
+                   basename(dir.doc)), "\n---\n", 
+            paste0(c(i-1, i, i+1), ": ", mock.line, sep = "\n"),
+            "---\n",
+            sep = "")
+        }
+      }
+    }
+  }
+  
   return(path.list)
 }

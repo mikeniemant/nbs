756d45cb57de0d9c9ef22bd77be0364624aab1aa
mikeniemant <m.niemantsverdriet@icloud.com>
2022-03-08 13:37:41 +0100

    Fix plotCalibration bug; bug when no value for the group parameter was provided

--- R/plotCalibration.R
+++ R/plotCalibration.R
@@ -8,11 +8,17 @@
 #' @return A ggplot containing the calibration plot
 #' @export
 plotCalibration <- function(x, plot_errorbar = T, plot_smooth = T, group = NULL, title = NULL) {
-  p <- ggplot2::ggplot(x, ggplot2::aes(x = mean_pred, y = obs_frac, colour = !!sym(group))) +
+  if(is.null(group)) {
+    p <- ggplot2::ggplot(x, ggplot2::aes(x = mean_pred, y = obs_frac))
+  } else {
+    p <- ggplot2::ggplot(x, ggplot2::aes(x = mean_pred, y = obs_frac, colour = !!sym(group)))
+  }
+
+  p <- p +
     ggplot2::geom_point() +
     ggplot2::geom_line() +
     ggplot2::geom_abline(ggplot2::aes(colour = "black", intercept = 0, slope = 1),
-                         show.legend = T, linetype = 2, alpha = 0.5) +
+                         show.legend = F, linetype = 2, alpha = 0.5) +
     ggplot2::labs(x = "Mean prediction",
                   y = "Fraction of positives",
                   colour = gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2",
@@ -22,6 +28,7 @@ plotCalibration <- function(x, plot_errorbar = T, plot_smooth = T, group = NULL,
     ggplot2::scale_x_continuous(limits = c(0, 1)) +
     ggplot2::scale_y_continuous(limits = c(0, 1)) +
     ggplot2::coord_equal()
+
 
   if(plot_errorbar) p <- p +
       ggplot2::geom_errorbar(ggplot2::aes(ymin = lci, ymax = uci), colour = "darkgrey", width = .02)
